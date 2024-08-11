bab16af1478c50d0ab4ba3db6b821ad35c3161f5
Joel Burget <joelburget@gmail.com>
2024-08-11 08:19:46 -0700

    Refactor main as hyperparameter sweep.

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
 from torch.distributions.normal import Normal
+from tqdm import tqdm
 from transformers import AutoTokenizer
 from transformer_lens import HookedTransformer
 from datasets import load_dataset
+import wandb
 
 model_name = "roneneldan/TinyStories-1M"
 ds_name = "roneneldan/TinyStories"
+
+sigmas = [0.01, 0.1, 1.0, 10.0, 100.0]
+l0_coefficients = [1e-3, 1e-2, 1e-1, 1.0, 10.0]
+expansion_factors = [8]
+learning_rates = [5e-6, 1e-5, 1e-4]
 
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = HookedTransformer.from_pretrained(model_name, device="cpu")
 ds = load_dataset(ds_name)
 
 input_dim = model.cfg.d_model
-expansion_factor = 8
-hidden_dim = input_dim * expansion_factor
-sigma = 1.0
-learning_rate = 1e-3
-beta = 1e-3
 hook_point = "blocks.4.hook_resid_post"
 
 normal = Normal(0, 1)
@@ -46,10 +47,7 @@ class SparseAutoencoder(nn.Module):
         return prob_non_zero.sum()
 
 
-sae = SparseAutoencoder(input_dim, hidden_dim, sigma)
-
-
-def train(model, sae, ds, learning_rate=learning_rate, beta=beta):
+def train(model, sae, ds, learning_rate, l0_coefficient):
     optimizer = torch.optim.Adam(sae.parameters(), lr=learning_rate)
     criterion = nn.MSELoss()
 
@@ -64,20 +62,47 @@ def train(model, sae, ds, learning_rate=learning_rate, beta=beta):
 
         reconstruction_loss = criterion(x_hat, x)
         l0_loss = sae.expected_l0_loss(h)
-        loss = reconstruction_loss + beta * l0_loss
+        loss = reconstruction_loss + l0_coefficient * l0_loss
 
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
-        i += 1
-        if i % 100 == 0:
-            print(
-                f"Step [{i}/10000], Loss: {loss.item():.4f}, Reconstruction Loss: {reconstruction_loss.item():.4f}, L0 Loss: {l0_loss.item():.6f}"
+        if i % 10 == 0:
+            wandb.log(
+                {
+                    "loss": loss.item(),
+                    "reconstruction_loss": reconstruction_loss.item(),
+                    "l0_loss": l0_loss.item(),
+                }
             )
-            if i % 1_000 == 0:
-                break
+        i += 1
 
 
 if __name__ == "__main__":
-    train(model, sae, ds)
+    for sigma in tqdm(sigmas, desc="sigma", position=0):
+        for l0_coefficient in tqdm(
+            l0_coefficients, desc="l0_coefficient", position=1, leave=False
+        ):
+            for expansion_factor in tqdm(
+                expansion_factors, desc="expansion_factor", position=2, leave=False
+            ):
+                for learning_rate in tqdm(
+                    learning_rates, desc="learning_rate", position=3, leave=False
+                ):
+                    wandb.init(
+                        project="sae_expected_l0",
+                        # track hyperparameters and run metadata
+                        config={
+                            "model_name": model_name,
+                            "dataset": ds_name,
+                            "sigma": sigma,
+                            "l0_coefficient": l0_coefficient,
+                            "expansion_factor": expansion_factor,
+                            "learning_rate": learning_rate,
+                        },
+                    )
+                    hidden_dim = input_dim * expansion_factor
+                    sae = SparseAutoencoder(input_dim, hidden_dim, sigma)
+                    train(model, sae, ds, learning_rate, l0_coefficient)
+                    wandb.finish()
