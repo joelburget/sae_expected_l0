fa06a1e21f08ef87d795eba75c7475a54c6b21d9
Joel Burget <joelburget@gmail.com>
2024-08-12 14:22:24 -0700

    Use wandb sweeps.

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.distributions.normal import Normal
-from tqdm import tqdm
 from transformers import AutoTokenizer
 from transformer_lens import HookedTransformer
 from datasets import load_dataset
@@ -11,20 +10,27 @@ import wandb
 model_name = "roneneldan/TinyStories-1M"
 ds_name = "roneneldan/TinyStories"
 
-sigmas = [0.01, 0.1, 1.0, 10.0, 100.0]
-l0_coefficients = [1e-3, 1e-2, 1e-1, 1.0, 10.0]
-expansion_factors = [8]
-learning_rates = [5e-6, 1e-5, 1e-4]
+expansion_factor = 8
 training_tokens = 8_000_000
 
 tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = HookedTransformer.from_pretrained(model_name, device="cpu")
+model = HookedTransformer.from_pretrained(model_name)
 ds = load_dataset(ds_name)
 
 input_dim = model.cfg.d_model
 hook_point = "blocks.4.hook_resid_post"
 
 normal = Normal(0, 1)
+
+sweep_configuration = {
+    "method": "bayes",
+    "metric": {"goal": "minimize", "name": "loss"},
+    "parameters": {
+        "l0_coefficient": {"min": 5e-4, "max": 0.1},
+        "sigma": {"min": 0.01, "max": 100.0},
+        "learning_rate": {"min": 1e-5, "max": 5e-3},
+    },
+}
 
 
 class SparseAutoencoder(nn.Module):
@@ -48,80 +54,65 @@ class SparseAutoencoder(nn.Module):
         return prob_non_zero.sum()
 
 
-def train(model, sae, ds, learning_rate, l0_coefficient):
-    optimizer = torch.optim.Adam(sae.parameters(), lr=learning_rate)
-    criterion = nn.MSELoss()
+def train(config=None):
+    with wandb.init(config=config):
+        config = wandb.config
+        learning_rate = config.learning_rate
+        l0_coefficient = config.l0_coefficient
+        sigma = config.sigma
+        hidden_dim = input_dim * expansion_factor
 
-    i = 0
-    total_tokens = 0
-    for input in ds["train"]:
-        try:
-            input = input["text"]
-            tokens = tokenizer(input)["input_ids"]
-            total_tokens += len(tokens)
-            _, cache = model.run_with_cache(torch.tensor(tokens), remove_batch_dim=True)
-            x = cache[hook_point]
+        sae = SparseAutoencoder(input_dim, hidden_dim, sigma)
+        optimizer = torch.optim.Adam(sae.parameters(), lr=learning_rate)
+        criterion = nn.MSELoss()
 
-            x_hat, h = sae(x)
-
-            reconstruction_loss = criterion(x_hat, x)
-            l0_loss = sae.expected_l0_loss(h)
-            loss = reconstruction_loss + l0_coefficient * l0_loss
-
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-
-            if i % 10 == 0:
-                wandb.log(
-                    {
-                        "loss": loss.item(),
-                        "reconstruction_loss": reconstruction_loss.item(),
-                        "l0_loss": l0_loss.item(),
-                        "total_tokens": total_tokens,
-                    }
+        i, total_tokens = 0, 0
+        for input in ds["train"]:
+            try:
+                input = input["text"]
+                tokens = tokenizer(input)["input_ids"]
+                total_tokens += len(tokens)
+                _, cache = model.run_with_cache(
+                    torch.tensor(tokens), remove_batch_dim=True
                 )
-            i += 1
-            if total_tokens > training_tokens:
-                break
-        except BaseException as e:
-            print(e)
-            pass
+                x = cache[hook_point]
+
+                x_hat, h = sae(x)
+
+                reconstruction_loss = criterion(x_hat, x)
+                l0_loss = sae.expected_l0_loss(h)
+                loss = reconstruction_loss + l0_coefficient * l0_loss
+
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+                log_info = {
+                    "loss": loss.item(),
+                    "reconstruction_loss": reconstruction_loss.item(),
+                    "l0_loss": l0_loss.item(),
+                    "total_tokens": total_tokens,
+                }
+                if total_tokens > training_tokens:
+                    wandb.log(log_info)
+                    break
+                if i % 10 == 0:
+                    wandb.log(log_info)
+                i += 1
+            except BaseException as e:
+                print(e)
+                pass
+
+        sae_save_path = "sae.pth"
+        torch.save(sae.state_dict(), sae_save_path)
+        wandb.save(sae_save_path)
+        artifact = wandb.Artifact(
+            f"sae-{sigma}-{l0_coefficient}-{expansion_factor}-{learning_rate}",
+            type="model",
+        )
+        artifact.add_file(sae_save_path)
 
 
 if __name__ == "__main__":
-    for sigma in tqdm(sigmas, desc="sigma", position=0):
-        for l0_coefficient in tqdm(
-            l0_coefficients, desc="l0_coefficient", position=1, leave=False
-        ):
-            for expansion_factor in tqdm(
-                expansion_factors, desc="expansion_factor", position=2, leave=False
-            ):
-                for learning_rate in tqdm(
-                    learning_rates, desc="learning_rate", position=3, leave=False
-                ):
-                    wandb.init(
-                        project="sae_expected_l0",
-                        # track hyperparameters and run metadata
-                        config={
-                            "model_name": model_name,
-                            "dataset": ds_name,
-                            "sigma": sigma,
-                            "l0_coefficient": l0_coefficient,
-                            "expansion_factor": expansion_factor,
-                            "learning_rate": learning_rate,
-                        },
-                    )
-                    hidden_dim = input_dim * expansion_factor
-                    sae = SparseAutoencoder(input_dim, hidden_dim, sigma)
-                    train(model, sae, ds, learning_rate, l0_coefficient)
-
-                    sae_save_path = "sae.pth"
-                    torch.save(sae.state_dict(), sae_save_path)
-                    wandb.save(sae_save_path)
-                    artifact = wandb.Artifact(
-                        f"sae-{sigma}-{l0_coefficient}-{expansion_factor}-{learning_rate}",
-                        type="model",
-                    )
-                    artifact.add_file(sae_save_path)
-                    wandb.finish()
+    sweep_id = wandb.sweep(sweep=sweep_configuration, project="sae-expected-l0-sweep")
+    wandb.agent(sweep_id, train, count=10)
